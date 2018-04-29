d298c950b84c0ac18d393a53e9f1a8fca031fd89
Devashish2910 <dgpatel2910@gmail.com>
2018-04-29 02:06:00 -0500

    initialized: Google oAuth2.0

--- back-end/controllers/oAuth.js
+++ back-end/controllers/oAuth.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// initalize strategy for Google oAuth2.0
+passport.use(new GoogleStrategy());
 
 // Google Authentication - /auth/google
 router.post('/google', (req, res) => {
