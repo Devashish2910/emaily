28cfb56cda318f7683d5f81f34547b216833e297
Devashish2910 <dgpatel2910@gmail.com>
2018-04-29 03:04:04 -0500

    implemented: Google oAuth2.0 with Passport

--- back-end/controllers/oAuth.js
+++ back-end/controllers/oAuth.js
@@ -2,14 +2,36 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require("./../config/key");
 
 // initalize strategy for Google oAuth2.0
-passport.use(new GoogleStrategy());
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: "/auth/google/callback"
+        },
+        (accessToken, refreshToken, profile, done) => {
+            console.log(accessToken);
+            console.log(refreshToken);
+            console.log(profile);
+        }
+    )
+);
+
+
 
 // Google Authentication - /auth/google
-router.post('/google', (req, res) => {
+router.get(
+    '/google',
+    passport.authenticate('google',{
+        scope: ['profile', 'email']
+    })
+);
 
-});
+// Callback URL - /auth/google/callback
+router.get('/google/callback', passport.authenticate('google'));
 
 // Export
 module.exports = router;
