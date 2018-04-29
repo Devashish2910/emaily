73f0a63f291e96f72d8ed28b4ba2a998e267d626
Devashish2910 <dgpatel2910@gmail.com>
2018-04-29 01:28:52 -0500

    modified: project structure

---
+++ back-end/controllers/oAuth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// Google Authentication - /auth/google
+router.post('/google', (req, res) => {
+
+});
+
+// Export
+module.exports = router;
--- back-end/routes/routes.js
+++ back-end/routes/routes.js
@@ -1,14 +1,17 @@
 // Import Controllers
 //const DriverController = require('./../controller/DriverController');
 //const RootController = require('./../controller/RootController');
+const oAuth = require('./../controllers/oAuth');
 
 
 module.exports = app => {
-    // Root Route
+    // Root
     app.get('/', (req, res) => {
         res.send({"message": "Hello, World!"})
     });
 
+    // Authentication
+    app.use('/auth', oAuth)
     // Routes on drivers
     //app.use('/d', DriverController);
 };
