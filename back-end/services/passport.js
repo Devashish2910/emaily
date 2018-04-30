7743ba26f37e75803eee0cb056564c3689b17e4b
Devashish2910 <dgpatel2910@gmail.com>
2018-04-30 02:24:19 -0500

    work on facebook login

--- back-end/services/passport.js
+++ back-end/services/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const FacebookStrategy = require('passport-facebook');
+const FacebookStrategy = require('passport-facebook').Strategy;
 const keys = require("./../config/key");
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
