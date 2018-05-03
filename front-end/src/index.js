78a4b150ce3c16a85e45ea5f07bde2e1127a4a17
Devashish2910 <dgpatel2910@gmail.com>
2018-05-03 00:20:59 -0500

    added: spripe config

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -14,3 +14,6 @@ ReactDOM.render(
     <Provider store={store}><App /></Provider>,
     document.querySelector('#root')
 );
+
+console.log(process.env.REACT_APP_STRIPE_PUBLIC_ID);
+console.log(process.env.NODE_ENV);
