b80c15820309973d2f664dadf43d5a899a4ddf20
Devashish2910 <dgpatel2910@gmail.com>
2018-05-03 07:03:08 -0500

    try luck

--- back-end/app.js
+++ back-end/app.js
@@ -22,11 +22,12 @@ require('./routes/routes')(app);
 
 // Set up for front-end
 if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+
     // send back main js/css file
-    app.use(express.static('../front-end/build'));
+    app.use(express.static(path.join(__dirname, '..front-end/build')));
 
     // return routes from index.html routes from react
-    const path = require('path');
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../front-end', 'build', 'index.html'));
     });
